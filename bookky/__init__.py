c1bbe7a39b6ac212161aba6b7cf3037b2c969816
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-10-24 11:05:20 +0200

    Delete sysvars

--- bookky/__init__.py
+++ bookky/__init__.py
@@ -14,8 +14,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.getenv('EMAIL_ADDRESS')
-app.config["MAIL_PASSWORD"] = os.getenv('EMAIL_PASSWORD')
+app.config["MAIL_USERNAME"] = 'BookkyWebsite@gmail.com'
+app.config["MAIL_PASSWORD"] = 'bookkyheroku'
 
 Session(app)
 bcrypt = Bcrypt(app)
