54ce502702def841407a4bcdc7772a51c3d4834f
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-10-24 11:01:12 +0200

    Edit Environment Variables

--- bookky/__init__.py
+++ bookky/__init__.py
@@ -14,8 +14,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get('EMAIL_ADDRESS')
-app.config["MAIL_PASSWORD"] = os.environ.get('EMAIL_PASSWORD')
+app.config["MAIL_USERNAME"] = os.getenv('EMAIL_ADDRESS')
+app.config["MAIL_PASSWORD"] = os.getenv('EMAIL_PASSWORD')
 
 Session(app)
 bcrypt = Bcrypt(app)
