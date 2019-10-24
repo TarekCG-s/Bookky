735ffd3801a27c616986151c870521e70ac5c8e0
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-10-24 12:44:25 +0200

    ADD SENDGRID

--- bookky/__init__.py
+++ bookky/__init__.py
@@ -5,21 +5,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_bcrypt import Bcrypt
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask_mail import Mail
+from flask_mail_sendgrid import MailSendGrid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd99ba4661b8599b8f4f139df838c9d8d'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = 'bookkywebsite@gmail.com'
-app.config["MAIL_PASSWORD"] = 'bookkyheroku'
+app.config['MAIL_SENDGRID_API_KEY'] ='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'
+# app.config["MAIL_SERVER"] = 'smtp.sendgrid.net'
+# app.config["MAIL_PORT"] = 587
+# app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USERNAME"] = 'bookkywebsite@gmail.com'
+# app.config["MAIL_PASSWORD"] = 'bookkyheroku'
 
 Session(app)
 bcrypt = Bcrypt(app)
-mail = Mail(app)
+mail = MailSendGrid(app)
 
 serializer = Serializer(app.config['SECRET_KEY'], 1800)
 
