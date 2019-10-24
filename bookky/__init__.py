47288b96fd896e6bb3681f0dae36f7d356f866c7
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-10-24 11:15:54 +0200

    UPDATE Config

--- bookky/__init__.py
+++ bookky/__init__.py
@@ -14,6 +14,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = 'BookkyWebsite@gmail.com'
 app.config["MAIL_PASSWORD"] = 'bookkyheroku'
 
